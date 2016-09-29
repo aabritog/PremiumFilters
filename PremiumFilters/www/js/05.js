984d2e8213492a1ab823f2abdac6786aed7f74d1
aabritog <ing.aabg@gmail.com>
2016-09-29 10:59:04 -0400

    Comentarios
    Se organiza y se agregan comentarios en el código.

--- PremiumFilters/www/js/05.js
+++ PremiumFilters/www/js/05.js
@@ -1,6 +1,10 @@
 
+//------------------------------------------------------------------------------
+//  BLOQUE DE FUNCIONES JQUERY
+//------------------------------------------------------------------------------
 $(document).ready(function () {
 
+    //Variables que contiene la ruta de los Web  Services a llamar.
     var webMethodGetTipoAplicacion = "http://a21287345-001-site1.etempurl.com/WCFPremiumFilters.asmx/GetTipoAplicacion";
     var webMethodGetMarcaVehiculo="http://a21287345-001-site1.etempurl.com/WCFPremiumFilters.asmx/GetMarcaVehiculo";
     var webMethodGetModelo="http://a21287345-001-site1.etempurl.com/WCFPremiumFilters.asmx/GetModelo";
@@ -10,23 +14,22 @@ $(document).ready(function () {
     var webMethodGetFuel="http://a21287345-001-site1.etempurl.com/WCFPremiumFilters.asmx/GetFFuel";
     var webMethodGetAC="http://a21287345-001-site1.etempurl.com/WCFPremiumFilters.asmx/GetFAirAC";
 
-          //var parameters= "{'sCountry':'Polonia'}";
-          
-          //SI TIENE PARAMETROS ESTA ES LA MANERA DE PASARLOS
-                /*var parameters = "{'idConcepto':'" + idConcepto + "','idConceptoRelacionado':'" + idConceptoRelacionado + "','idRelacion':'" + idRelacion + "','idCardinalidad':'" + idCardinalidad + "','idCardinalidadRelacionada':'" + idCardinalidadRelacionada + "','idOntologia':'" + idOntologia + "'}";*/
-        //LLAMADO A JSON AJAX LIBRERIA DE JQUERY
-        $.ajax({
-                type: "POST",
-                url: webMethodGetTipoAplicacion,
-                //data: parameters,
-                contentType: "application/json; charset=utf-8",
-                dataType: "json",
-                success: llenarTipoAplicacion,
-                error: function (XMLHttpRequest, textStatus, errorThrown) {
-                    alert(textStatus + ": " + XMLHttpRequest.responseText);
-                }
-        });
 
+    //Objeto AJAX que se encarga de obtener la informacíon del combo "Tipo Aplicación".
+    $.ajax({
+            type: "POST",
+            url: webMethodGetTipoAplicacion,
+            //data: parameters,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: llenarTipoAplicacion,
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert(textStatus + ": " + XMLHttpRequest.responseText);
+            }
+    });
+
+    
+    //Evento jQuery que se encarga de llamar objeto AJAX que obtiene la informacíon del combo "Marca Vehículo".
     $("#select1").change(function(){
         var idTipoAplicacion = document.getElementById("select1").value;
         var parametrosGetMarcaVehiculo="{'Id_TMk':'"+idTipoAplicacion+"'}";
@@ -43,6 +46,8 @@ $(document).ready(function () {
         });
     });
 
+
+    //Evento jQuery que se encarga de llamar objeto AJAX que obtiene la informacíon del combo "Modelo".
     $("#select2").change(function(){
         var idMarcaVehiculo = document.getElementById("select2").value;
         //console.log(idMarcaVehiculo);
@@ -60,6 +65,8 @@ $(document).ready(function () {
         });
     }); 
 
+
+    //Evento jQuery que se encarga de llamar objeto AJAX que obtiene la informacíon del combo "Cilindraje - Motor/Capacidad".
     $("#select3").change(function(){
         var idTipoAplicacion= document.getElementById("select1").value
         var idModelo = document.getElementById("select3").value;
@@ -78,19 +85,21 @@ $(document).ready(function () {
         });
     }); 
 
+
+    //Evento jQuery que se encarga de llamar los objetos AJAX que van a obtener la información de cada una de las grillas de resultados.
     $("#select4").change(function(){
+
         var idModelo= document.getElementById("select3").value
         var idCilindraje = document.getElementById("select4").value;
         var motCap;
-        
        
-
         if (idCilindraje != '0'){
             motCap="";
         }
         else{
             motCap= $("#select4").text();
         }
+
         var parametrosGetAir = "{'Id_RefMk':'" + idModelo + "','McilL':'" + idCilindraje + "','MotCap':'"+ motCap +"'}";
         var parametrosGetOil = parametrosGetAir;
         var parametrosGetFuel = parametrosGetAir;
@@ -98,6 +107,7 @@ $(document).ready(function () {
         
         //console.log(idMarcaVehiculo);
          
+        //Objeto AJAX para la grilla de los Filtros de Aire.
         $.ajax({
                 type: "POST",
                 url: webMethodGetAir,
@@ -110,6 +120,7 @@ $(document).ready(function () {
                 }
         });
 
+        //Objeto AJAX para la grilla de los Filtros de Aceite.
         $.ajax({
                 type: "POST",
                 url: webMethodGetOil,
@@ -122,6 +133,7 @@ $(document).ready(function () {
                 }
         });
 
+        //Objeto AJAX para la grilla de los Filtros de Combustible.
         $.ajax({
                 type: "POST",
                 url: webMethodGetFuel,
@@ -134,6 +146,7 @@ $(document).ready(function () {
                 }
         });
 
+        //Objeto AJAX para la grilla de los Filtros de Aire Acondicionado.        
         $.ajax({
                 type: "POST",
                 url: webMethodGetAC,
@@ -150,6 +163,12 @@ $(document).ready(function () {
     });   
 });
 
+
+//------------------------------------------------------------------------------
+//  BLOQUE DE FUNCIONES JAVASCRIPT
+//------------------------------------------------------------------------------
+
+//Función que se encarga de llenar transformar el resultado de formato string a formato Json.
 function IsJsonString(str) {
     try {
         JSON.parse(str);
@@ -159,6 +178,8 @@ function IsJsonString(str) {
     return true;
 }
 
+
+//Función que se encarga de llenar el combo "Tipo Aplicación" con la información obtenida del objeto AJAX.
 function llenarTipoAplicacion(result) {
     
     var objJson = JSON.parse(result.d);
@@ -171,6 +192,8 @@ function llenarTipoAplicacion(result) {
     });
 }
 
+
+//Función que se encarga de llenar el combo "Marca Vehiculo" con la información obtenida del objeto AJAX.
 function llenarMarcaVehiculo(result) {
     //console.log(result.d);
     var objJsonMarcaVehiculo = JSON.parse(result.d);
@@ -179,11 +202,12 @@ function llenarMarcaVehiculo(result) {
     $.each(objJsonMarcaVehiculo, function (i, item) {
         //console.log(item.Id_TMk); 
         HtmlMarcaVehiculo += "<option value='" + item.Id_Mk + "'>" + item.Marca + "</option>";
-        
     });
     $("#select2").html(HtmlMarcaVehiculo);
 }
 
+
+//Función que se encarga de llenar el combo "Modelo" con la información obtenida del objeto AJAX.
 function llenarModelo(result) {
     //console.log(result.d);
     var objJsonModelo = JSON.parse(result.d);
@@ -192,11 +216,12 @@ function llenarModelo(result) {
     $.each(objJsonModelo, function (i, item) {
         //console.log(item.Id_TMk); 
         HtmlModelo += "<option value='" + item.Id_RefMk + "'>" + item.Ref_Marca + "</option>";
-        
     });
     $("#select3").html(HtmlModelo);
 }
 
+
+//Función que se encarga de llenar el combo "Motor/Capacidad - Cilindraje" con la información obtenida del objeto AJAX.
 function llenarCilindraje(result) {
     //console.log(result.d);
     var objJsonCilindraje = JSON.parse(result.d);
@@ -214,12 +239,13 @@ function llenarCilindraje(result) {
         $("#lblCilindraje").html('Motor/Capacidad:');
     }
 
-        
     });
-    $("#select4").html(HtmlCilindraje);
 
+    $("#select4").html(HtmlCilindraje);
 }
 
+
+//Función que se encarga de llenar la grila de los Filtros de Aire con la información obtenida del objeto AJAX.
 function llenarAir(result) {
     //console.log(result.d);
         if (IsJsonString(result.d)){
@@ -228,12 +254,9 @@ function llenarAir(result) {
         var objJsonAir = JSON.parse(result.d);
         var HtmlAir;
 
-        
         $.each(objJsonAir, function (i, item) {
             //console.log(objJsonAir); 
             HtmlAir+="<tr><td><a id='"+item.AIRE+"' src='#' onclick='llenarFiltro(this.id);'>"+item.AIRE+"</a></td><td>"+item.Maño+"</td></tr>"
-            
-            
         });
         $("#tbodyAir").html(HtmlAir);
     }
@@ -242,8 +265,71 @@ function llenarAir(result) {
     }
 }
 
-    function llenarFiltro(id) {
 
+//Función que se encarga de llenar la grila de los Filtros de Aceite con la información obtenida del objeto AJAX.
+function llenarOil(result) {
+    //console.log(result.d);
+    if(IsJsonString(result.d)){
+        $("#pnlOil").show();
+        var objJsonOil = JSON.parse(result.d);
+        var HtmlOil;
+        
+        $.each(objJsonOil, function (i, item) {
+            //console.log(objJsonAir); 
+            HtmlOil+="<tr><td><a id='"+item.ACEITE+"' src='#' onclick='llenarFiltro(this.id);'>"+item.ACEITE+"</a></td><td>"+item.Maño+"</td></tr>"
+        });
+        $("#tbodyOil").html(HtmlOil);
+    }
+    else{
+        $("#pnlOil").hide();
+    }
+}
+
+
+//Función que se encarga de llenar la grila de los Filtros de Combustible con la información obtenida del objeto AJAX.
+function llenarFuel(result) {
+    //console.log(result.d);
+    if(IsJsonString(result.d)){
+
+        $("#pnlFuel").show();
+        var objJsonFuel = JSON.parse(result.d);
+        var HtmlFuel;
+        
+        $.each(objJsonFuel, function (i, item) {
+            //console.log(objJsonAir); 
+            HtmlFuel+="<tr><td><a id='"+item.COMBUSTIBLE+"' src='#' onclick='llenarFiltro(this.id);'>"+item.COMBUSTIBLE+"</a></td><td>"+item.Maño+"</td></tr>"
+        });
+        $("#tbodyFuel").html(HtmlFuel);
+    }
+    else{
+        $("#pnlFuel").hide();
+    }
+}
+
+
+//Función que se encarga de llenar la grila de los Filtros de Aire Acondicionado con la información obtenida del objeto AJAX.
+function llenarAC(result) {
+    //console.log(result.d);
+    if (IsJsonString(result.d)){
+        $("#pnlAC").show();
+        var objJsonAC = JSON.parse(result.d);
+        var HtmlAC;
+        
+        $.each(objJsonAC, function (i, item) {
+            //console.log(objJsonAir); 
+            HtmlAC+="<tr><td><a id='"+item.AIREAC+"' src='#' onclick='llenarFiltro(this.id);'>"+item.AIREAC+"</a></td><td>"+item.Maño+"</td></tr>"
+        });
+        $("#tbodyAC").html(HtmlAC);
+    }
+    else{
+        $("#pnlAC").hide();
+        //alert(result.d);
+    }
+}
+
+
+//Función que se encarga de llamar objeto AJAX para obtener la información detallada del filtro seleccionado de cualquiera de las grillas.
+function llenarFiltro(id) {
     var webMethodGetFiltro="http://a21287345-001-site1.etempurl.com/WCFPremiumFilters.asmx/GetFiltro";
     var parametrosGetFiltro="{'PF_Ref':'"+id+"'}";
         $.ajax({
@@ -257,23 +343,18 @@ function llenarAir(result) {
                     alert(textStatus + ": " + XMLHttpRequest.responseText);
                 }
         });
-    //console.log(result.d);
-    
-    
-
+        //console.log(result.d);
 }
 
+
+//Función que se encarga de llenar el modal con los detalles del filtro seleccionado de cualquiera de las grillas.
 function detalleFiltro(result){
     var objJsonFiltro = JSON.parse(result.d);
     var HtmlFiltro;
-     var rutaImgFiltro= "http://premiumfilters.com.co/ImagesFilters/";
+    var rutaImgFiltro= "http://premiumfilters.com.co/ImagesFilters/";
 
-     
-
-    
     $.each(objJsonFiltro, function (i, item) {
         //console.log(objJsonAir); 
-        
         $("#imgFiltro").attr("src",rutaImgFiltro+item.IMG);
         $("#ref").html(item.PF_Ref);
         $("#linea").html(item.RefLinea);
@@ -282,73 +363,8 @@ function detalleFiltro(result){
         $("#largo").html(item.DLargo);
         $("#alto").html(item.DAl);
         $("#rosca").html(item.DAncho);
-
-        
     });
-
     $('#myModal').modal('show');
-}
-
-function llenarOil(result) {
-    console.log(result.d);
-    if(IsJsonString(result.d)){
-        $("#pnlOil").show();
-        var objJsonOil = JSON.parse(result.d);
-        var HtmlOil;
-        
-        $.each(objJsonOil, function (i, item) {
-            //console.log(objJsonAir); 
-            HtmlOil+="<tr><td><a id='"+item.ACEITE+"' src='#' onclick='llenarFiltro(this.id);'>"+item.ACEITE+"</a></td><td>"+item.Maño+"</td></tr>"
-            
-            
-        });
-        $("#tbodyOil").html(HtmlOil);
-    }
-    else{
-        $("#pnlOil").hide();
-    }
-}
-
-function llenarFuel(result) {
-    //console.log(result.d);
-    if(IsJsonString(result.d)){
-
-        $("#pnlFuel").show();
-        var objJsonFuel = JSON.parse(result.d);
-        var HtmlFuel;
-        
-        $.each(objJsonFuel, function (i, item) {
-            //console.log(objJsonAir); 
-            HtmlFuel+="<tr><td><a id='"+item.COMBUSTIBLE+"' src='#' onclick='llenarFiltro(this.id);'>"+item.COMBUSTIBLE+"</a></td><td>"+item.Maño+"</td></tr>"
-            
-            
-        });
-        $("#tbodyFuel").html(HtmlFuel);
-    }
-    else{
-        $("#pnlFuel").hide();
-    }
-}
-
-function llenarAC(result) {
-    //console.log(result.d);
-    if (IsJsonString(result.d)){
-        $("#pnlAC").show();
-        var objJsonAC = JSON.parse(result.d);
-        var HtmlAC;
-        
-        $.each(objJsonAC, function (i, item) {
-            //console.log(objJsonAir); 
-            HtmlAC+="<tr><td><a id='"+item.AIREAC+"' src='#' onclick='llenarFiltro(this.id);'>"+item.AIREAC+"</a></td><td>"+item.Maño+"</td></tr>"
-            
-            
-        });
-        $("#tbodyAC").html(HtmlAC);
-    }
-    else{
-        $("#pnlAC").hide();
-        //alert(result.d);
-    }
 }
 
 
